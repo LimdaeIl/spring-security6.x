de72e52948f4bdf746147eed6e26461e49dc4dbe
LimDae94 <piay801@naver.com>
2024-04-09 11:27:05 +0900

    인증 컨텍스트 - SecurityContext / SecurityContextHolder

--- security/src/main/java/book/security/IndexController.java
+++ security/src/main/java/book/security/IndexController.java
@@ -1,17 +1,27 @@
 package book.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final SecurityContextService securityContextService;
 
     @GetMapping("/")
     public String index() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("authentication = " + authentication);
+
+        securityContextService.securityContext();
         return "index";
     }
 
